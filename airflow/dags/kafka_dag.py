3a20462a9b6089bfdc89527889cdcae962bec6b7
Daniel Rodriguez <141448979+danielde720@users.noreply.github.com>
2023-11-07 21:03:28 -0800

    Update kafka_dag.py

--- airflow/dags/kafka_dag.py
+++ airflow/dags/kafka_dag.py
@@ -1,30 +1,58 @@
 from airflow import DAG
-from airflow.decorators import task, dag
-from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+import subprocess
 
-@dag(schedule_interval='@daily', start_date=datetime(2023, 11, 3), catchup=False)
-def kafka_cassandra_pipeline():
+def run_python_script(script_path):
+    subprocess.run(["python", script_path], check=True)
 
-    @task
-    def produce_data():
-        
-        pass
+def produce_data():
+    run_python_script("/opt/airflow/scripts/kafka_stream.py")
 
-    @task
-    def validate_data():
-        
-        pass
+def validate_data():
+    run_python_script("/opt/airflow/scripts/data_validation.py")
 
-    @task
-    def consume_data():
-        
-        pass
 
-    produce_task = produce_data()
-    validate_task = validate_data()
-    consume_task = consume_data()
+def consume_data():
+    run_python_script("/opt/airflow/scripts/consumer.py")
 
-    produce_task >> validate_task >> consume_task
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 11, 6),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=1),
+    'catchup': False
+}
 
-# Assign the DAG to a variable to indicate it should be discovered by Airflow
-kafka_cassandra_dag = kafka_cassandra_pipeline()
+dag = DAG(
+    'kafka_cassandra_pipeline',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+produce_task = PythonOperator(
+    task_id='produce_data',
+    python_callable=produce_data,
+    dag=dag,
+)
+
+validate_task = PythonOperator(
+    task_id='validate_data',
+    python_callable=validate_data,
+    dag=dag,
+)
+
+consume_task = PythonOperator(
+    task_id='consume_data',
+    python_callable=consume_data,
+    retries=5,  # Number of retries for the consume task
+    retry_delay=timedelta(seconds=5),  # Delay between retries for the consume task
+    dag=dag,
+)
+
+# Set tasks to run independently
+produce_task
+validate_task
+consume_task
+
